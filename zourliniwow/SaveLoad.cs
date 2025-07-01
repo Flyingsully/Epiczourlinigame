9368540fad713e6b1fcbe16eeebeaad4b9604785
Flyingsully <Fredslynn@gmail.com>
2025-07-01 10:46:48 +1000

    Added Save/Load

--- zourliniwow/SaveLoad.cs
+++ zourliniwow/SaveLoad.cs
@@ -1,6 +1,27 @@
 using Godot;
 using System;
+using System.Runtime.CompilerServices;
 
 public partial class SaveLoad : Node
 {
+    private const string SaveLocation = "user://SaveFile.save";
+
+    int HighScore = 0;
+    int CurrentScore = 0;
+
+    private void SaveScore()
+    {
+        using var file = FileAccess.Open(SaveLocation, FileAccess.ModeFlags.WriteRead);
+        file.Store32((uint)HighScore);
+    }
+    private void LoadScore()
+    {
+        if (FileAccess.FileExists(SaveLocation)) ;
+        {
+            using var file = FileAccess.Open(SaveLocation, FileAccess.ModeFlags.Read);
+            HighScore = (int)file.Get32();
+        }
+
+    }
+    
 }
